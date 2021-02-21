53993176ecaa679288c5bd33a6b66115e251d1fe
kintrix <boldizsar02@gmail.com>
2021-02-21 20:57:52 +0100

    Fixed packages, improved logging of launcher

--- launch.py
+++ launch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os, sys
 from datetime import datetime
 import json
@@ -5,19 +6,27 @@ import time
 
 root = os.path.dirname(os.path.realpath(__file__))
 
-CRASH_LOG_DIR = f"{root}/crash_logs"
+CRASH_LOG_DIR = os.path.join(root, "crash_logs")
 
 def main():
-    os.system("./node_modules/typescript/bin/tsc")
+    print("-- compiling... --")
+    tsc_exit_code = os.system("./node_modules/typescript/bin/tsc")
+    if tsc_exit_code != 0:
+        print(f"tsc stopped with a non zero exit code ({tsc_exit_code})")
+        exit(1)
+    print("-- compile successful --")
 
     iter = 0
 
+    print("-- finding entry point... --")
     with open("package.json", "r") as f:
         entry_point = json.loads(f.read())["main"]
-    
+    print(f"-- found entry point ('{entry_point}') --")
+
     if not os.path.exists(CRASH_LOG_DIR):
         os.mkdir(CRASH_LOG_DIR)
 
+    print("-- launching bot --")
     while True:
         exit_code = os.system(f"node {entry_point}")
         current_time = datetime.now().strftime("%Y-%m-%d - %H:%M:%S")
@@ -26,7 +35,7 @@ def main():
         with open(f"{CRASH_LOG_DIR}/crash{iter}.log", "w") as f:
             f.write(crash_log)
         print("-- waiting to restart bot... --")
-        time.sleep(5) # wait 5 seconds before restarting
+        time.sleep(5)       # wait 5 seconds before restarting
         print("-- restarting bot... --")
         iter += 1
 
